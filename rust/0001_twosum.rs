1243572f76ac8e84206e867c3219b277c90cf09a
allester <allester@umail.ucsb.edu>
2023-03-04 14:58:50 -0800

    Create 0001_twosum.rs

---
+++ rust/0001_twosum.rs
@@ -0,0 +1,20 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut d: HashMap<i32, usize> = HashMap::with_capacity(nums.len());
+
+        for (i, num) in nums.into_iter().enumerate() {
+            let x = target - num;
+            match(d.get(&x)) {
+                Some(&index) => {
+                    return vec![index as i32, i as i32];
+                }
+                None => {
+                    d.insert(num, i);
+                }
+            }
+        }
+        return vec![];
+    }
+}
