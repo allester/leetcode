e4bf65ab58fa4ca0fdfa60fdc19c53286df0369d
allester <allester@ramayrat.com>
2023-06-30 11:25:21 -0700

    Create 20_valid_parentheses.py

---
+++ python/20_valid_parentheses.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        d = {'(' : ')', '{' : '}', '[' : ']'}
+        stack = []
+        for char in s:
+            if char in d:
+                stack.append(char)
+                continue
+
+            if not stack:
+                return False
+
+            else:
+                last = stack.pop()
+                if char != d[last]:
+                    return False
+        return len(stack) == 0
