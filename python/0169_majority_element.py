7b53097b465bf4ad300bf98e3aa83202e43bfa8a
allester <allester@ramayrat.com>
2023-08-08 13:27:06 -0700

    Update 0169_majority_element.py
    optimized from O(nlogn) time to O(n) time and O(n) space

--- python/0169_majority_element.py
+++ python/0169_majority_element.py
@@ -1,7 +1,14 @@
 '''
-https://leetcode.com/problems/majority-element/submissions/1005021692/
+https://leetcode.com/problems/majority-element/submissions/1016024226/
 '''
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        return sorted(nums)[len(nums)//2]
+        d = dict()
+        for num in nums:
+            d[num] = d.get(num, 0) + 1
+        m = len(nums) / 2
+        for key, value in d.items():
+            if value > m:
+                return key
+        return -1
