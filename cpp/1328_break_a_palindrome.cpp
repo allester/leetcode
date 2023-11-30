5c71723164552748dcb5ecc9f4544de4ed2105ad
allester <allester@ramayrat.com>
2023-11-30 10:35:02 -0800

    Create 1328_break_a_palindrome.cpp

---
+++ cpp/1328_break_a_palindrome.cpp
@@ -0,0 +1,21 @@
+/*
+https://leetcode.com/problems/break-a-palindrome/submissions/1109728772/
+*/
+
+class Solution {
+public:
+    string breakPalindrome(string palindrome) {
+        int n = palindrome.size();
+        if (n == 1) {
+            return "";
+        }
+        for (int i = 0; i < n / 2; i++) {
+            if (palindrome[i] != 'a') {
+                palindrome[i] = 'a';
+                return palindrome;
+            }
+        }
+        palindrome[n -1] = 'b';
+        return palindrome;
+    }
+};
