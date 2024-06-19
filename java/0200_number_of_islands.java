1ef4eb99e8dc019a3bd13dd2ed631bd43b1925d0
allester <allester@ramayrat.com>
2024-06-19 14:03:23 -0700

    0200 java

---
+++ java/0200_number_of_islands.java
@@ -0,0 +1,28 @@
+/*
+https://leetcode.com/problems/number-of-islands/submissions/1293973938/
+*/
+class Solution {
+    public int numIslands(char[][] grid) {
+        int islands = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    islands++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return islands;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || grid.length - 1 < i || j < 0 || grid[0].length - 1 < j || grid[i][j] == '0') {
+            return;
+        }
+        grid[i][j] = '0';
+        dfs(grid, i-1, j);
+        dfs(grid, i, j-1);
+        dfs(grid, i+1, j);
+        dfs(grid, i, j+1);
+    }
+}
